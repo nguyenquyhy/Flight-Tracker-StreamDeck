b0d5bfc44e711f53ad00718327d3ba7e476ca519
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-03-20 17:55:59 +0800

    Add project files.

---
+++ FlightStreamDeck.AddOn/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using FlightStreamDeck.Logics;
+using System.Windows;
+
+namespace FlightStreamDeck.AddOn
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DeckLogic deckLogic;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            deckLogic = new DeckLogic();
+            deckLogic.KeyPressed += DeckLogic_KeyPressed;
+        }
+
+        private void DeckLogic_KeyPressed(object sender, System.EventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                MessageBox.Show(this, $"Key pressed");
+            });
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            deckLogic.Initialize();
+        }
+    }
+}
---
+++ FlightStreamDeck.Logics/DeckLogic.cs
@@ -0,0 +1,22 @@
+﻿using OpenMacroBoard.SDK;
+using StreamDeckSharp;
+using System;
+
+namespace FlightStreamDeck.Logics
+{
+    public class DeckLogic
+    {
+        public event EventHandler KeyPressed;
+
+        public void Initialize()
+        {
+            var device = StreamDeck.OpenDevice();
+            device.KeyStateChanged += Device_KeyStateChanged;
+        }
+
+        private void Device_KeyStateChanged(object sender, KeyEventArgs e)
+        {
+            KeyPressed?.Invoke(this, new EventArgs());
+        }
+    }
+}
