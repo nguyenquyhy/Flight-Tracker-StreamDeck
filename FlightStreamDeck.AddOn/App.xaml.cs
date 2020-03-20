f8914bf7607841096ec7a3d140cf13d3670ad779
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-03-20 19:31:37 +0800

    Add facility for logging and DI

--- FlightStreamDeck.AddOn/App.xaml.cs
+++ FlightStreamDeck.AddOn/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System.IO;
 using System.Windows;
 
 namespace FlightStreamDeck.AddOn
@@ -13,5 +11,48 @@ namespace FlightStreamDeck.AddOn
     /// </summary>
     public partial class App : Application
     {
+        public ServiceProvider ServiceProvider { get; private set; }
+        public IConfigurationRoot Configuration { get; private set; }
+
+        private MainWindow mainWindow = null;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Debug()
+                .MinimumLevel.Information()
+                .WriteTo.File("flightstreamdeck.log", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 3)
+                .CreateLogger();
+
+            //services.AddOptions<AppSettings>().Bind(Configuration).ValidateDataAnnotations();
+
+            services.AddLogging(configure =>
+            {
+                configure.AddSerilog();
+            });
+
+            //services.AddSingleton<IFlightConnector, SimConnectFlightConnector>();
+            //services.AddSingleton(new UserPreferencesLoader("preferences.json"));
+
+            services.AddTransient(typeof(MainWindow));
+        }
     }
 }
