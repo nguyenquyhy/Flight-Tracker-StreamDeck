1e7bd4bbd5f051a8ab2948dc255d79b2b72bc620
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-03-25 20:37:42 +0800

    Show and swap values for NavComButton

--- FlightStreamDeck.Logics/Actions/NavComAction.cs
+++ FlightStreamDeck.Logics/Actions/NavComAction.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using FlightStreamDeck.Core;
+using Newtonsoft.Json.Linq;
 using SharpDeck;
 using SharpDeck.Events.Received;
 using System;
@@ -9,14 +10,21 @@ namespace FlightStreamDeck.Logics.Actions
 {
     class NavComAction : StreamDeckAction
     {
+        private const int HOLD_DURATION_MILLISECONDS = 1000;
         private readonly IImageLogic imageLogic;
+        private readonly IFlightConnector flightConnector;
         private readonly Timer timer;
         private string device;
+        private string type;
+        private TOGGLE_VALUE? active;
+        private TOGGLE_VALUE? standby;
+        private TOGGLE_EVENT? toggle;
 
-        public NavComAction(IImageLogic imageLogic)
+        public NavComAction(IImageLogic imageLogic, IFlightConnector flightConnector)
         {
             this.imageLogic = imageLogic;
-            timer = new Timer { Interval = 1000 };
+            this.flightConnector = flightConnector;
+            timer = new Timer { Interval = HOLD_DURATION_MILLISECONDS };
             timer.Elapsed += Timer_Elapsed;
         }
 
@@ -30,10 +38,21 @@ namespace FlightStreamDeck.Logics.Actions
 
         protected override async Task OnWillAppear(ActionEventArgs<AppearancePayload> args)
         {
+            flightConnector.GenericValuesUpdated += FlightConnector_GenericValuesUpdated;
             device = args.Device;
 
-            var type = args.Payload.Settings.Value<string>("Type");
+            type = args.Payload.Settings.Value<string>("Type");
             await SetImageAsync(imageLogic.GetNavComImage(type));
+
+            SwitchTo(type);
+        }
+
+        protected override Task OnWillDisappear(ActionEventArgs<AppearancePayload> args)
+        {
+            flightConnector.GenericValuesUpdated -= FlightConnector_GenericValuesUpdated;
+            SwitchTo(null);
+
+            return Task.CompletedTask;
         }
 
         protected override Task OnKeyDown(ActionEventArgs<KeyPayload> args)
@@ -48,14 +67,86 @@ namespace FlightStreamDeck.Logics.Actions
             {
                 timer.Stop();
                 // Transfer
+
+                if (toggle != null)
+                {
+                    flightConnector.Toggle(toggle.Value);
+                }
             }
             return Task.CompletedTask;
         }
 
         protected override async Task OnSendToPlugin(ActionEventArgs<JObject> args)
         {
-            var type = args.Payload.Value<string>("Type");
+            type = args.Payload.Value<string>("Type");
             await SetImageAsync(imageLogic.GetNavComImage(type));
+        }
+
+        private async void FlightConnector_GenericValuesUpdated(object sender, ToggleValueUpdatedEventArgs e)
+        {
+            string value1 = null, value2 = null;
+            if (active != null && e.GenericValueStatus.ContainsKey(active.Value))
+            {
+                value1 = e.GenericValueStatus[active.Value];
+            }
+            if (standby != null && e.GenericValueStatus.ContainsKey(standby.Value))
+            {
+                value2 = e.GenericValueStatus[standby.Value];
+            }
+
+            await SetImageAsync(imageLogic.GetNavComImage(type, value1, value2));
+        }
+
+        private void SwitchTo(string type)
+        {
+            if (active != null)
+            {
+                flightConnector.DeRegisterSimValue(active.Value);
+            }
+            if (standby != null)
+            { 
+                flightConnector.DeRegisterSimValue(standby.Value);
+            }
+            switch (type)
+            {
+                case "NAV1":
+                    active = TOGGLE_VALUE.NAV_ACTIVE_FREQUENCY__1;
+                    standby = TOGGLE_VALUE.NAV_STANDBY_FREQUENCY__1;
+                    toggle = TOGGLE_EVENT.NAV1_RADIO_SWAP;
+                    break;
+                case "NAV2":
+                    active = TOGGLE_VALUE.NAV_ACTIVE_FREQUENCY__2;
+                    standby = TOGGLE_VALUE.NAV_STANDBY_FREQUENCY__2;
+                    toggle = TOGGLE_EVENT.NAV2_RADIO_SWAP;
+                    break;
+                case "COM1":
+                    active = TOGGLE_VALUE.COM_ACTIVE_FREQUENCY__1;
+                    standby = TOGGLE_VALUE.COM_STANDBY_FREQUENCY__1;
+                    toggle = TOGGLE_EVENT.COM_STBY_RADIO_SWAP;
+                    break;
+                case "COM2":
+                    active = TOGGLE_VALUE.COM_ACTIVE_FREQUENCY__2;
+                    standby = TOGGLE_VALUE.COM_STANDBY_FREQUENCY__2;
+                    toggle = TOGGLE_EVENT.COM2_RADIO_SWAP;
+                    break;
+                default:
+                    active = null;
+                    standby = null;
+                    toggle = null;
+                    break;
+            }
+            if (active != null)
+            {
+                flightConnector.RegisterSimValue(active.Value);
+            }
+            if (standby != null)
+            { 
+                flightConnector.RegisterSimValue(standby.Value);
+            }
+            if (toggle != null)
+            {
+                flightConnector.RegisterToggleEvent(toggle.Value);
+            }
         }
     }
 }
--- FlightStreamDeck.Logics/ImageLogic.cs
+++ FlightStreamDeck.Logics/ImageLogic.cs
@@ -12,7 +12,7 @@ namespace FlightStreamDeck.Logics
     {
         string GetImage(string text, bool active, string value = null);
         string GetNumberImage(int number);
-        string GetNavComImage(string type);
+        string GetNavComImage(string type, string value1 = null, string value2 = null);
     }
 
     public class ImageLogic : IImageLogic
@@ -70,17 +70,32 @@ namespace FlightStreamDeck.Logics
             return "data:image/png;base64, " + base64;
         }
 
-        public string GetNavComImage(string type)
+        public string GetNavComImage(string type, string value1 = null, string value2 = null)
         {
             var font = SystemFonts.CreateFont("Arial", 17, FontStyle.Regular);
+            var valueFont = SystemFonts.CreateFont("Arial", 13, FontStyle.Regular);
 
-            var text = type.ToUpperInvariant();
             Image img = backGround;
             using var img2 = img.Clone(ctx =>
             {
                 var imgSize = ctx.GetCurrentSize();
-                var size = TextMeasurer.Measure(text, new RendererOptions(font));
-                ctx.DrawText(text, font, Color.White, new PointF(imgSize.Width / 2 - size.Width / 2, imgSize.Height / 4));
+
+                if (type != null)
+                {
+                    var size = TextMeasurer.Measure(type, new RendererOptions(font));
+                    ctx.DrawText(type, font, Color.White, new PointF(imgSize.Width / 2 - size.Width / 2, imgSize.Height / 4));
+                }
+
+                if (!string.IsNullOrWhiteSpace(value1))
+                {
+                    var size1 = TextMeasurer.Measure(value1, new RendererOptions(valueFont));
+                    ctx.DrawText(value1, valueFont, Color.Yellow, new PointF(imgSize.Width / 2 - size1.Width / 2, imgSize.Height / 2));
+                }
+                if (!string.IsNullOrWhiteSpace(value2))
+                {
+                    var size2 = TextMeasurer.Measure(value2, new RendererOptions(valueFont));
+                    ctx.DrawText(value2, valueFont, Color.White, new PointF(imgSize.Width / 2 - size2.Width / 2, imgSize.Height / 2 + size2.Height + 2));
+                }
             });
             using var memoryStream = new MemoryStream();
             img2.Save(memoryStream, new PngEncoder());
