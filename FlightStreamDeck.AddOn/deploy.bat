e323837e190d1444665ac930eca49b3a5c905e8b
Nguyen Quy Hy <nguyenquyhy@live.com.sg>
2020-12-29 22:48:54 -0500

    Add Start command to deploy.bat

--- FlightStreamDeck.AddOn/deploy.bat
+++ FlightStreamDeck.AddOn/deploy.bat
@@ -1,2 +1,3 @@
 taskkill -f -t -im StreamDeck.exe -fi "status eq running"
 XCOPY "." "%appdata%\Elgato\StreamDeck\Plugins\tech.flighttracker.streamdeck.sdPlugin\" /S /E /Y
+START /d "%ProgramW6432%\Elgato\StreamDeck" StreamDeck.exe
