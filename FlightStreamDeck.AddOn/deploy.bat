7b6b617d0d69be6ebb8648e9f4f3cb49e2e30471
Thierry Bigot <thierrybigot@outlook.com>
2021-09-25 19:19:19 +0200

    build event improvements

--- FlightStreamDeck.AddOn/deploy.bat
+++ FlightStreamDeck.AddOn/deploy.bat
@@ -1,3 +1,4 @@
 taskkill -f -t -im StreamDeck.exe -fi "status eq running"
-XCOPY "." "%appdata%\Elgato\StreamDeck\Plugins\tech.flighttracker.streamdeck.sdPlugin\" /S /E /Y
-START /d "%ProgramW6432%\Elgato\StreamDeck" StreamDeck.exe
+taskkill -f -t -im FlightStreamDeck.AddOn.exe -fi "status eq running"
+XCOPY "." "%appdata%\Elgato\StreamDeck\Plugins\tech.flighttracker.streamdeck.sdPlugin\" /S /E /Y /F
+::START /d "%ProgramW6432%\Elgato\StreamDeck" StreamDeck.exe
