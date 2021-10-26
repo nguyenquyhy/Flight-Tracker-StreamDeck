9aab10b004e6ce3067d12912c8041d03f2107c47
egpd1701 <egpd1701@users.noreply.github.com>
2021-10-26 19:32:09 +0100

    Added some events

---
+++ IFlightConnector.cs
@@ -0,0 +1,135 @@
+﻿using FlightStreamDeck.Core;
+using System;
+using System.Collections.Generic;
+
+namespace FlightStreamDeck.Logics
+{
+    public interface IFlightConnector
+    {
+        event EventHandler<AircraftStatusUpdatedEventArgs> AircraftStatusUpdated;
+        event EventHandler<ToggleValueUpdatedEventArgs> GenericValuesUpdated;
+        void ApOff();
+        void ApOn();
+        void ApToggle();
+        void ApHdgToggle();
+        void ApNavToggle();
+        void ApAprToggle();
+        void ApAltToggle();
+        void ApVsToggle();
+        void ApFlcOn();
+        void ApFlcOff();
+
+        /// <param name="heading">In Degree</param>
+        void ApHdgSet(uint heading);
+        void ApHdgInc();
+        void ApHdgDec();
+
+        /// <param name="altitude">In Feet</param>
+        void ApAltSet(uint altitude);
+        void ApAltInc();
+        void ApAltDec();
+
+        /// <param name="speed">In Feet per min</param>
+        void ApVsSet(uint speed);
+
+        void ApAirSpeedSet(uint speed);
+        void ApAirSpeedInc();
+        void ApAirSpeedDec();
+        void QNHSet(uint qnh);
+        void QNHInc();
+        void QNHDec();
+
+        void AvMasterToggle(uint state);
+
+        void Trigger(ToggleEvent setAction, uint data = 0);
+
+        void RegisterToggleEvent(ToggleEvent toggleAction);
+
+        void RegisterSimValues(List<ToggleValue> simValues);
+        void DeRegisterSimValues(List<ToggleValue> simValues);
+    }
+
+    public class AircraftStatusUpdatedEventArgs : EventArgs
+    {
+        public AircraftStatusUpdatedEventArgs(AircraftStatus aircraftStatus)
+        {
+            AircraftStatus = aircraftStatus;
+        }
+
+        public AircraftStatus AircraftStatus { get; }
+    }
+
+    public class ToggleValueUpdatedEventArgs : EventArgs
+    {
+        public ToggleValueUpdatedEventArgs(List<ToggleValue> genericValueStatus)
+        {
+            GenericValueStatus = genericValueStatus;
+        }
+
+        public List<ToggleValue> GenericValueStatus { get; }
+    }
+
+    public class AircraftStatus
+    {
+        public string Callsign { get; set; }
+
+        public double SimTime { get; set; }
+        public int? LocalTime { get; set; }
+        public int? ZuluTime { get; set; }
+        public long? AbsoluteTime { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Altitude { get; set; }
+        public double AltitudeAboveGround { get; set; }
+
+        public double Heading { get; set; }
+        public double TrueHeading { get; set; }
+
+        public double GroundSpeed { get; set; }
+        public double IndicatedAirSpeed { get; set; }
+        public double VerticalSpeed { get; set; }
+
+        public double FuelTotalQuantity { get; set; }
+
+        public double Pitch { get; set; }
+        public double Bank { get; set; }
+
+        public bool IsOnGround { get; set; }
+        public bool StallWarning { get; set; }
+        public bool OverspeedWarning { get; set; }
+
+        public bool IsAutopilotOn { get; set; }
+
+        public bool IsApHdgOn { get; set; }
+        public int ApHeading { get; set; }
+
+        public bool IsApNavOn { get; set; }
+
+        public bool IsApAprOn { get; set; }
+
+        public bool IsApAltOn { get; set; }
+        public int ApAltitude0 { get; set; }
+        public int ApAltitude1 { get; set; }
+
+        public bool IsApVsOn { get; set; }
+        public int ApVs { get; set; }
+
+        public bool IsApFlcOn { get; set; }
+        public int ApAirspeed { get; set; }
+
+        public int QNHMbar { get; set; }
+
+        public string Transponder { get; set; }
+        public int FreqencyCom1 { get; set; }
+        public int FreqencyCom2 { get; set; }
+        public bool IsAvMasterOn { get; set; }
+        public double Nav1OBS { get; set; }
+        public double Nav2OBS { get; set; }
+        public double ADFCard { get; set; }
+        public int ADFActiveFrequency1 { get; set; }
+        public int ADFStandbyFrequency1 { get; set; }
+        public int ADFActiveFrequency2 { get; set; }
+        public int ADFStandbyFrequency2 { get; set; }
+    }
+}
