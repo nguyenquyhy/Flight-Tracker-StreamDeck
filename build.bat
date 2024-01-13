d8299ee96695cfde4e2825168ded6ed3b74eb38c
mithrk <steffenmackie@gmail.com>
2024-01-13 12:27:58 +0100

    I wanted to turn on/off the landing / taxi lights for the TBM 930.
    This did not work with the toggles. and the TBM's lights are on a 3 way switch.
    
    These settings are apparently also linked to the light potentiometers, and in addition to turning the landing lights ON, one must turn the taxi lights OFF with the same action, and also change the potentiometers 2 and 10.
    
    To this end I added a toggle variable to GenericToggle, and added a new Generic Toggle where 4 actions can be defined.
    
    In order to set the TBM's landing / taxi lights:
    First button, 4 events  -  4 parameters:
    
    LDG ON:
    
    LANDING_LIGHTS_SET   -    1
    LIGHT_POTENTIOMETER_10_SET  -   100
    LIGHT_POTENTIOMETER_2_SET    -   0
    TAXI_LIGHTS_SET                            -   0
    
    Feedback value: LIGHT LANDING
    
    TAXI ON:
    
    LANDING_LIGHTS_SET    -   0
    TAXI_LIGHTS_SET              -  1
    LIGHT_POTENTIOMETER_10_SET - 0
    LIGHT_POTENTIOMETER_2_SET   -  100
    
    Hold Event:
    TAXI_LIGHTS_SET     0
    
    Feedback Value :  LIGHT TAXI
    
    ----------------------------------------------
    
    This enables a single press to turn on Landing Light, a press on Taxi light to turn on Taxi light, and holding Taxi light down turns off the Taxi light.
    This configuration mimics the 3 way switch in the TBM 930.
    
    Tested on Steam Deck +
    
    Also I have no idea what i'm doing, haven't worked with C# or HTML before, github is only a funny word to me, and I installed VisualStudio just because I wanted more actions on the button. For future reference to myself if I'm considering looking at an open source program - this took me 12 hours to figure out and I only *think* I figured it out.

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 rd build /s /q
 mkdir build
+XCopy DistributionTool.exe build\DistributionTool.exe*
 dotnet publish FlightStreamDeck.AddOn\FlightStreamDeck.AddOn.csproj -c Release -r win10-x64 --self-contained
 XCopy FlightStreamDeck.AddOn\bin\Release\net7.0-windows\win10-x64\publish build\tech.flighttracker.streamdeck.sdPlugin /e /h /c /i
 cd build
 DistributionTool.exe -b -i tech.flighttracker.streamdeck.sdPlugin -o .
 cd ..
+pause
